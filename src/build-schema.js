fecc58cf62a6d017a17431350d186c1b521dfa97
Bruce Herr II <bherr@indiana.edu>
2018-06-05 09:12:12 -0400

    Replace cibridge version via package.json. Move version down to 0.1.0.

--- src/build-schema.js
+++ src/build-schema.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 const package = require('../package.json');
 const typeDefs = require('./schema');
 
-fs.writeFileSync(package.schema, typeDefs);
+fs.writeFileSync(package.schema.replace('VERSION', package.version), typeDefs);
