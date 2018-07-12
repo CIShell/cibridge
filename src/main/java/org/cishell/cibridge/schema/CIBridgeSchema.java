a08bb9f628fb86aef1181f13bc5cedc68f781efe
Bruce Herr II <bherr@indiana.edu>
2018-07-12 08:46:41 -0400

    * Added continuous build with Travis CI * Added a Java class for java applications to load the latest cibridge schema string from java code

---
+++ src/main/java/org/cishell/cibridge/schema/CIBridgeSchema.java
@@ -0,0 +1,33 @@
+package org.cishell.cibridge.schema;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+
+public class CIBridgeSchema {
+    public static final String schemaString = CIBridgeSchema.entryToString("/cibridge-schema.graphqls");
+
+    private static String entryToString(String entryPath) {
+        try {
+            InputStream inputStream = CIBridgeSchema.class.getResourceAsStream(entryPath);
+            final int bufferSize = 1024;
+            final char[] buffer = new char[bufferSize];
+            final StringBuilder out = new StringBuilder();
+            Reader in = new InputStreamReader(inputStream, "UTF-8");
+            for (;;) {
+                int rsz = in.read(buffer, 0, buffer.length);
+                if (rsz < 0)
+                    break;
+                out.append(buffer, 0, rsz);
+            }
+            return out.toString();
+        } catch(Exception e) {
+            return "";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(CIBridgeSchema.schemaString);
+    }
+}
